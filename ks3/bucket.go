9d9c2519f55b5797c2d94263de89aa8718ca6339
likui2 <likui2@kingsoft.com>
2024-01-19 10:06:05 +0800

    fix: RestoreObjectDetail调整

--- ks3/bucket.go
+++ ks3/bucket.go
@@ -831,13 +831,6 @@ func (bucket Bucket) RestoreObject(objectKey string, options ...Option) error {
 
 // RestoreObjectDetail support more features than RestoreObject
 func (bucket Bucket) RestoreObjectDetail(objectKey string, restoreConfig RestoreConfiguration, options ...Option) error {
-	if restoreConfig.JobParameters != nil {
-		if restoreConfig.JobParameters.Tier == "" {
-			// Expedited, Standard, Bulk
-			restoreConfig.JobParameters.Tier = string(RestoreStandard)
-		}
-	}
-
 	if restoreConfig.Days == 0 {
 		restoreConfig.Days = 1
 	}
